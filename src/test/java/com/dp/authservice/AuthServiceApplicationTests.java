97706a0f5d20281612ad1020833019b8fa012b0b
dynamic-project <mabdullahrakib@gmail.com>
2025-01-15 23:16:14 +0600

    Initial commit: Authentication Service setup

---
+++ src/test/java/com/dp/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
